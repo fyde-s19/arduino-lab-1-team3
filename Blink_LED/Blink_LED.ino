ae19a8bdc3d3d766aebc5ac6884dac261f98e8a8
Molly Hu <mollyhu@wireless-10-145-159-228.public.utexas.edu>
2019-02-18 16:49:10 -0600

    Dimmer

--- Blink_LED/Blink_LED.ino
+++ Blink_LED/Blink_LED.ino
@@ -11,7 +11,13 @@ void timedBlink(int interval) {
   delay(1000);                       // wait for a second
 }
 
+void LED_Blink() {
+  for(int i = 1; i <= 3; i++){
+    timedBlink(i);    
+  }
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
-  timedBlink(3);
+  LED_Blink();
 }
